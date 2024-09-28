55c2968a76659aad339a03607c703135c51f38fd
Robin Dong <robin_dongbin@outlook.com>
2024-09-28 18:27:30 +0800

    laravel permission

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -11,7 +11,7 @@ class UserPolicy
      */
     public function viewAny(?User $user): bool
     {
-        return true;
+        return $user->hasRole('admin');
     }
 
     /**
@@ -19,7 +19,7 @@ class UserPolicy
      */
     public function view(User $user, User $model): bool
     {
-        return false;
+        return $user->hasRole('admin');
     }
 
     /**
@@ -27,7 +27,7 @@ class UserPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return $user->hasRole('admin');
     }
 
     /**
@@ -35,7 +35,7 @@ class UserPolicy
      */
     public function update(User $user, User $model): bool
     {
-        return false;
+        return $user->hasRole('admin');
     }
 
     /**
@@ -43,7 +43,7 @@ class UserPolicy
      */
     public function delete(User $user, User $model): bool
     {
-        return false;
+        return $user->hasRole('admin');
     }
 
     /**
@@ -51,7 +51,7 @@ class UserPolicy
      */
     public function restore(User $user, User $model): bool
     {
-        return false;
+        return $user->hasRole('admin');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
 Route::redirect('/login', '/')->name('login');
+
+Route::get('/test', function () {
+    logger('test', ['test' => 'test']);
+});
