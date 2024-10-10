9b99b8f3c47c6f40105e7a28d823850f4cd4acfb
Robin Dong <robin_dongbin@outlook.com>
2024-10-10 11:42:30 +0800

    Revert "filament health check"
    This reverts commit 952b50102d6eb0d68f6b0c25b10253231238cee1.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,7 +18,6 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use ShuvroRoy\FilamentSpatieLaravelHealth\FilamentSpatieLaravelHealthPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -59,7 +58,6 @@ class AdminPanelProvider extends PanelProvider
             ->maxContentWidth(MaxWidth::Full)
             ->spa()
             ->plugins([
-                FilamentSpatieLaravelHealthPlugin::make(),
             ]);
     }
 
