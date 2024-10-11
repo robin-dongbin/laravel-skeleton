06894ea6e21573e99c1715d97c3b821d718698a2
Robin Dong <robin_dongbin@outlook.com>
2024-10-11 10:13:29 +0800

    重构用户表结构并合并迁移文件
    - 在初始迁移文件中添加了更多用户字段：username、nickname、avatar、phone_number等
    - 对用户表进行以下变更： - 将 name 字段设置为可空并添加索引 - 允许 email 字段为空
      - 添加 username、nickname、avatar 和 phone_number 字段 - 为 nickname 和 phone_number 添加验证字段
    - 删除了不必要的更新迁移文件，将变更合并到创建用户的初始迁移中

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->index()->nullable();
+            $table->string('username')->unique();
+            $table->string('nickname')->index();
+            $table->string('avatar')->nullable();
+            $table->string('phone_number')->unique()->nullable();
+            $table->string('phone_number_verified_at')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
