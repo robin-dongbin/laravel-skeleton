08494dadabdd57f7181cdd99c7c553c650418d0e
Robin Dong <robin_dongbin@outlook.com>
2025-01-20 10:04:35 +0800

    refactor(database): 优化 role_user 表结构并更新依赖
    - 调整 role_user 表中 user_id 和 role_id 字段的定义
    - 移除不必要的索引
    - 更新 composer.json 中的依赖版本
      - 删除 archtechx/enums
      -将 workerman/workerman 从4.2 升级到5.0

--- database/migrations/2024_10_14_014907_create_role_user_table.php
+++ database/migrations/2024_10_14_014907_create_role_user_table.php
@@ -9,11 +9,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('role_id');
+            $table->foreignId('user_id')->index()->constrained();
+            $table->foreignId('role_id')->constrained();
             $table->timestamps();
-
-            $table->index('user_id');
         });
     }
 
