73eb353f0c960440f903219c2a923b1cf044bf88
Robin Dong <robin_dongbin@outlook.com>
2025-02-08 21:25:15 +0800

    build: 更新 Rector 配置以适配框架结构
    - 修改了 Rector 的路径配置，仅包含必要的目录
    - 启用了多个代码质量提升的预设集
    - 添加了导入名称的配置

--- rector.php
+++ rector.php
@@ -3,18 +3,26 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\EarlyReturn\Rector\Return_\ReturnBinaryOrToEarlyReturnRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap',
-        __DIR__.'/config',
-        __DIR__.'/lang',
-        __DIR__.'/public',
-        __DIR__.'/resources',
+        __DIR__.'/bootstrap/app.php',
+        __DIR__.'/database',
     ])
-    // uncomment to reach your current PHP version
-//    ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withImportNames(importDocBlockNames: false, importShortClasses: false, removeUnusedImports: true)
+    ->withSkip([
+        ReturnBinaryOrToEarlyReturnRector::class,
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+    ])
+    ->withPhpSets();
