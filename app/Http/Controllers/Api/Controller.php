30e65b654591ab3f4f8f681a3c7c01461d328484
Robin Dong <robin_dongbin@outlook.com>
2025-02-08 16:16:58 +0800

    style(lang): 格式化语言文件
    - 调整了多个语言文件的格式，使其更加规范
    - 统一了数组键值对的样式
    - 删除了多余的空行和空格

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-
 abstract class Controller
 {
     public function limit(): int
--- app/Http/Controllers/Workerman/Controller.php
+++ app/Http/Controllers/Workerman/Controller.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Workerman;
 
-
 use App\Models\User;
 use Workerman\Protocols\Http\Request;
 
@@ -11,6 +10,7 @@ class Controller
     public function index(Request $request)
     {
         $user = User::find(1);
+
         return response()->json($user);
     }
 }
