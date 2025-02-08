9f5807d087b0232ae7002bcfceeb5de42db891f7
Robin Dong <robin_dongbin@outlook.com>
2025-02-08 15:37:13 +0800

    refactor(admin): 重构用户管理接口并添加队列配置
    - 移除了 Knuckles\Scribe相关的注解，简化了代码结构
    - 更新了 Dedoc\Scramble 注解，改为使用 Group 属性
    - 删除了 versionable 配置文件，移除了版本控制相关设置
    - 新增了 queue 配置文件，设置了队列服务相关配置

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,16 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Dedoc\Scramble\Attributes\Group;
 use Dedoc\Scramble\Attributes\QueryParameter;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Pagination\LengthAwarePaginator;
-use Knuckles\Scribe\Attributes\Endpoint;
-use Knuckles\Scribe\Attributes\Group;
-use Knuckles\Scribe\Attributes\Response;
-use Knuckles\Scribe\Attributes\ResponseFromApiResource;
 
-#[Group('Users')]
+#[Group('User')]
 class UserController extends Controller
 {
     /**
@@ -32,15 +29,11 @@ class UserController extends Controller
         return UserResource::collection($users);
     }
 
-    #[Endpoint('Store')]
-    #[ResponseFromApiResource(UserResource::class, User::class)]
     public function store()
     {
         //
     }
 
-    #[Endpoint('Show')]
-    #[ResponseFromApiResource(UserResource::class, User::class)]
     public function show($id)
     {
         $user = User::findOrFail($id);
@@ -48,15 +41,11 @@ class UserController extends Controller
         return UserResource::make($user);
     }
 
-    #[Endpoint('Update')]
-    #[ResponseFromApiResource(UserResource::class, User::class)]
     public function update()
     {
         //
     }
 
-    #[Endpoint('Destroy')]
-    #[Response(status: 204)]
     public function destroy($id)
     {
         $user = User::findOrFail($id);
