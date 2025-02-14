9cf2de92e9f234fac0161396c826a04fe67e8e0c
Robin Dong <robin_dongbin@outlook.com>
2025-02-14 18:31:24 +0800

    feat(layout): 优化导航栏展示逻辑
    - 在头部加入 Application标识，增强品牌认知
    - 实现侧边栏在路由切换时自动关闭的功能
    -调整导航栏组件布局，提高空间利用率

--- resources/js/layouts/AuthenticatedLayout.tsx
+++ resources/js/layouts/AuthenticatedLayout.tsx
@@ -2,14 +2,18 @@ import NavLinks from '@/components/Layout/NavLinks'
 import ThemeToggle from '@/components/Layout/ThemeManager'
 import UserAvatar from '@/components/Layout/UserAvatar'
 import BlankLayout from '@/layouts/BlankLayout'
-import { Link, usePage } from '@inertiajs/react'
+import { Link, router, usePage } from '@inertiajs/react'
 import { AppShell, Burger, UnstyledButton } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import React from 'react'
 
 export default function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
   const { navigation } = usePage().props
-  const [opened, { toggle }] = useDisclosure()
+  const [opened, { toggle, close }] = useDisclosure()
+
+  router.on('finish', () => {
+    close()
+  })
 
   return (
     <BlankLayout>
@@ -24,10 +28,12 @@ export default function AuthenticatedLayout({ children }: { children: React.Reac
       >
         <AppShell.Header>
           <div className="flex h-full items-center justify-between px-4">
-            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-            <UnstyledButton component={Link} href={route('admin.home')} className="text-2xl font-bold">
-              Application
-            </UnstyledButton>
+            <div className="flex items-center gap-4">
+              <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+              <UnstyledButton component={Link} href={route('admin.home')} className="text-2xl font-bold">
+                Application
+              </UnstyledButton>
+            </div>
             <div className="flex items-center gap-4">
               <ThemeToggle />
               <UserAvatar />
