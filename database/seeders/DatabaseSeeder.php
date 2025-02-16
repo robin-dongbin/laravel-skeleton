01fec36ff2c5f436cd0318670dcdb79e22a44bea
Robin Dong <robin_dongbin@outlook.com>
2025-02-16 17:17:46 +0800

    refactor(seeders): 移除 RoleSeeder 并在 DatabaseSeeder 中实现角色创建
    - 删除了 RoleSeeder 类
    - 在 DatabaseSeeder 中直接实现角色创建逻辑
    - 更新了 .env.example 文件，将数据库连接改为 sqlite
    - 在 .gitignore 中添加了 laravel 目录

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call([
-            RoleSeeder::class,
-        ]);
+        foreach (RoleName::cases() as $role) {
+            Role::create(['name' => $role]);
+        }
 
         $user = User::factory()
             ->create([
