b5d79a154ce843510bdabdfdf3012e7b96d575e1
Robin Dong <robin_dongbin@outlook.com>
2025-02-25 11:05:16 +0800

    refactor: 删除未使用的模型和路由
    - 删除了 app/Models/Model.php 文件，该文件未被使用
    - 在 routes/web.php 中移除了测试路由，该路由不再需要

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,5 @@
 
 use Dedoc\Scramble\Scramble;
 
-Route::get('/test', function () {
-    return now()->toDateTimeString();
-});
 Scramble::registerUiRoute(path: 'docs/admin', api: 'admin');
 Scramble::registerJsonSpecificationRoute(path: 'docs/admin.json', api: 'admin');
