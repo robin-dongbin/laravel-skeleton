49bae0906378bbd940896a4bb41ce48b6c79572d
Robin Dong <robin_dongbin@outlook.com>
2025-03-22 21:02:36 +0800

    feat(RequestLog): 添加数据批量删除功能
    - 引入 MassPrunable 特性
    - 实现 prunable 方法，用于删除一周前的日志记录

--- app/Models/RequestLog.php
+++ app/Models/RequestLog.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RequestLog extends Model
 {
-    use HasUuids;
+    use HasUuids, MassPrunable;
 
     protected function casts(): array
     {
@@ -17,6 +19,11 @@ class RequestLog extends Model
             'payload' => 'json',
             'response' => 'json',
         ];
+    }
+
+    public function prunable(): RequestLog|Builder
+    {
+        return static::where('created_at', '<=', now()->subDays(7));
     }
 
     public function user(): BelongsTo
