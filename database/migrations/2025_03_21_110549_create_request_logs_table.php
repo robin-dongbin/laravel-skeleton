47a4a45a28f32889f3b354d9ed8765c6b1bc6ed3
Robin Dong <robin_dongbin@outlook.com>
2025-04-18 08:56:19 +0800

    refactor(database): 移除 request_logs 表中的 client_request_id 字段
    - 从 request_logs表中删除了 client_request_id 字段
    - 保留了其他字段的结构不变

--- database/migrations/2025_03_21_110549_create_request_logs_table.php
+++ database/migrations/2025_03_21_110549_create_request_logs_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('request_logs', function (Blueprint $table) {
             $table->id();
             $table->uuid()->unique();
-            $table->string('client_request_id')->nullable();
             $table->foreignId('user_id')->nullable()->constrained();
             $table->ipAddress()->nullable();
             $table->string('method', 6)->nullable();
