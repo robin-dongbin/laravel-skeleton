36c597d61be21404b14d2f8b8e51a42ec59555b2
Robin Dong <robin_dongbin@outlook.com>
2025-04-19 13:31:18 +0800

    refactor(web): 优化路由配置
    - 移除了不必要的路由定义代码
    - 合并了 admin 路由的定义，简化为一行
    - 删除了未使用的 ip 路由

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,4 @@ use Dedoc\Scramble\Scramble;
 Scramble::registerUiRoute(path: 'docs/admin', api: 'admin');
 Scramble::registerJsonSpecificationRoute(path: 'docs/admin.json', api: 'admin');
 
-Route::get('ip', function () {
-    return request()->ips();
-});
-
-Route::view('admin', 'admin');
-Route::view('admin/{any}', 'admin')->where('any', '.*');
+Route::view('admin{any}', 'admin')->where('any', '.*');
