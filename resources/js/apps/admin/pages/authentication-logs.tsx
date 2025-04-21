94d6e15cb3d75d4f05e64dd81cfa2cf3a22a31f6
Robin Dong <robin_dongbin@outlook.com>
2025-04-21 08:04:59 +0800

    feat(admin): 添加认证日志页面
    - 创建新的认证日志页面组件
    - 实现日志数据的获取和展示
    - 添加日志过滤功能，支持路径、状态码和方法筛选

---
+++ resources/js/apps/admin/pages/authentication-logs.tsx
@@ -0,0 +1,79 @@
+import type { AdminRequestLogsIndexResponse, RequestLogResource } from '#admin/types/api'
+import PageContainer from '@/packages/components/PageContainer'
+import { FilterPanel, ResourceTable } from '@/packages/components/ResourceTable'
+import { useQueryBuilder } from '@/packages/hooks/useQueryBuilder'
+import { request } from '@/packages/lib/request'
+import { Select, TextInput } from '@mantine/core'
+import type { DataTableColumn } from 'mantine-datatable'
+import { type ClientLoaderFunctionArgs, useLoaderData } from 'react-router'
+
+export const clientLoader = (args: ClientLoaderFunctionArgs) => request<AdminRequestLogsIndexResponse>(args)
+
+const columns: DataTableColumn<RequestLogResource>[] = [
+  {
+    accessor: 'method',
+    title: 'method',
+  },
+  {
+    accessor: 'path',
+    title: 'path',
+  },
+  {
+    accessor: 'response_status',
+    title: 'response_status',
+  },
+  {
+    accessor: 'duration',
+    title: 'duration',
+    sortable: true,
+  },
+  {
+    accessor: 'memory',
+    title: 'memory',
+    sortable: true,
+  },
+  {
+    accessor: 'created_at',
+    title: 'created_at',
+    sortable: true,
+  },
+]
+
+export default function Users() {
+  const { data } = useLoaderData<typeof clientLoader>()
+
+  const query = useQueryBuilder<{
+    path: string
+    response_status: string | null
+    method: string | null
+  }>({
+    path: '',
+    response_status: null,
+    method: null,
+  })
+
+  return (
+    <PageContainer>
+      <FilterPanel query={query}>
+        <TextInput label="Path" {...query.getInputProps('filter.path')}></TextInput>
+        <Select
+          label="Status"
+          data={['200', '201', '204', '400', '401', '403', '404', '422', '500', '503']}
+          clearable
+          {...query.getInputProps('filter.response_status')}
+        ></Select>
+        <Select
+          label="method"
+          data={['GET', 'POST', 'PUT', 'PATCH', 'DELETE']}
+          {...query.getInputProps('filter.method')}
+        ></Select>
+      </FilterPanel>
+      <ResourceTable<RequestLogResource>
+        columns={columns}
+        records={data?.data}
+        totalRecords={data?.meta.total}
+        query={query}
+      />
+    </PageContainer>
+  )
+}
