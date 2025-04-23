b538e208ce8b51f78aab9e765e016a0d362de756
Robin Dong <robin_dongbin@outlook.com>
2025-04-23 19:18:24 +0800

    refactor(app): 优化 IP 创建逻辑
    - 使用 updateOrCreate 方法替换原有的查询和创建逻辑
    -简化代码结构，提高代码可读性和性能

--- app/Actions/CreateIpAction.php
+++ app/Actions/CreateIpAction.php
@@ -9,19 +9,10 @@ class CreateIpAction
 {
     public function handle(array $data): Ip
     {
-        $address = data_get($data, 'address');
-
-        $ip = Ip::query()->where('address', $address)->first();
-
-        if (! $ip) {
-            $ip = Ip::create([
-                'address' => $address,
-                'location' => data_get($data, 'location'),
-                'status' => data_get($data, 'status', IpStatus::Active),
-                'remark' => data_get($data, 'remark'),
-            ]);
-        }
-
-        return $ip;
+        return Ip::updateOrCreate(['address' => data_get($data, 'address')], [
+            'location' => data_get($data, 'location'),
+            'status' => data_get($data, 'status', IpStatus::Active),
+            'remark' => data_get($data, 'remark'),
+        ]);
     }
 }
