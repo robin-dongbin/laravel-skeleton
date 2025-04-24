bbf3cab1b262cd5ae5bbf6d6a03f31184fea75b1
Robin Dong <robin_dongbin@outlook.com>
2025-04-24 18:33:54 +0800

    feat(console): 添加应用部署命令- 新增 DeployCommand 类实现应用部署功能 - 部署过程包括安装依赖、执行部署相关命令、重启 PHP 服务和构建前端 - 通过 git diff检查文件变更来决定是否执行安装或构建操作 -优化部署流程，提高部署效率

---
+++ app/Console/Commands/DeployCommand.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
+use Throwable;
+
+class DeployCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:deploy';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '部署应用';
+
+    /**
+     * @throws Throwable
+     */
+    public function handle(): void
+    {
+        $this->info('开始部署');
+
+        $this->installVendor();
+        $this->executeDeployCommand();
+        $this->reloadPHP();
+        $this->buildFrontEnd();
+
+        $this->info('部署成功');
+    }
+
+    private function installVendor(): void
+    {
+        $result = Process::run("git diff --name-only HEAD~1 HEAD | grep -E '^(composer\\.json$|composer.\\.lock$)'");
+
+        if ($result->successful() && trim($result->output()) !== '') {
+            $this->info('PHP包管理文件有变更，执行安装...');
+            $result = Process::run('composer install --no-interaction --prefer-dist --optimize-autoloader');
+            echo $result;
+        } else {
+            $this->info('PHP包管理文件无变更, 跳过安装');
+        }
+    }
+
+    private function executeDeployCommand(): void
+    {
+        $this->call('migrate', ['--force' => true]);
+        $this->call('optimize');
+        $this->call('horizon:terminate');
+    }
+
+    /**
+     * @throws Throwable
+     */
+    private function reloadPHP(): void
+    {
+        if (Process::run('echo "" | sudo -S service php8.3-fpm reload')) {
+            $this->info('PHP 已重启');
+        } else {
+            $this->fail('PHP 重启失败');
+        }
+    }
+
+    /**
+     * @throws Throwable
+     */
+    private function buildFrontEnd(): void
+    {
+        $result = Process::run("git diff --name-only HEAD~1 HEAD | grep -E '^(resources/|package\\.json$|pnpm-lock\\.yaml$)'");
+
+        if ($result->successful() && trim($result->output()) !== '') {
+            $this->info('前端文件或包管理文件有变更，执行构建...');
+            $this->runInstall();
+        } else {
+            $this->info('前端文件和包管理文件无变更，跳过构建。');
+        }
+    }
+
+    /**
+     * @throws Throwable
+     */
+    private function runInstall(): void
+    {
+        $result = Process::timeout(5000)->run('pnpm install --frozen-lockfile --ignore-scripts');
+
+        echo $result->output();
+
+        if ($result->successful()) {
+            $this->info('安装成功');
+            $this->runBuild();
+        } else {
+            $this->fail('安装失败');
+        }
+    }
+
+    /**
+     * @throws Throwable
+     */
+    private function runBuild(): void
+    {
+        $result = Process::timeout(5000)->run('pnpm run build');
+
+        echo $result->output();
+
+        if ($result->successful()) {
+            $this->info('构建成功');
+        } else {
+            $this->fail('构建失败');
+        }
+    }
+}
