1bcc78b56a6fe78d7feadf74762212235e3e3a99
Robin Dong <robin_dongbin@outlook.com>
2025-05-03 10:48:11 +0800

    feat(admin): 添加代码扫描工具- 引入 react-scan包 - 配置 scan函数以启用代码扫描

--- resources/js/apps/admin/main.tsx
+++ resources/js/apps/admin/main.tsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import { scan } from 'react-scan'
 import App from './App'
 import './bootstrap'
 import './i18n'
+
+scan({
+  enabled: true,
+})
 
 const root = document.getElementById('root')
 
