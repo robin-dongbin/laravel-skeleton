b9a76d6211cba5c08dde82113ce5b51fa3bd660a
robin-dongbin <robin_dongbin@outlook.com>
2025-05-22 15:08:01 +0800

    refactor(components): 移除 MediaPicker 组件
    移除了 MediaPicker 组件及其相关文件，包括：- MediaPicker.tsx
    - UppyDashboard.tsx
    - ResourceGrid.tsx
    - index.ts 中的 Media 和 ResourceGrid 导出这个改动可能是为了简化项目结构或使用其他替代方案。

--- resources/js/packages/components/ResourceTable/index.ts
+++ resources/js/packages/components/ResourceTable/index.ts
@@ -1,4 +1,3 @@
 export { default as AdvancedFilter } from './AdvancedFilter'
-export { default as ResourceGrid } from './ResourceGrid'
 export { default as ResourceTable } from './ResourceTable'
 export { default as TabFilter } from './TabFilter'
