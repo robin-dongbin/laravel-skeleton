eb69ca6abeafba650c3f4694e1e46a98e1a227c9
robin-dongbin <robin_dongbin@outlook.com>
2025-10-17 12:22:50 +0800

    feat: 更新 Prettier 和 ESLint 配置，优化代码格式化和检查规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 import js from '@eslint/js'
 import prettier from 'eslint-config-prettier'
 import react from 'eslint-plugin-react'
-import reactCompiler from 'eslint-plugin-react-compiler'
 import reactHooks from 'eslint-plugin-react-hooks'
+import { defineConfig } from 'eslint/config'
 import globals from 'globals'
 import ts from 'typescript-eslint'
 
-export default ts.config(
+export default defineConfig(
   { ignores: ['vendor', 'node_modules', 'public'] },
   {
     extends: [js.configs.recommended, ts.configs.recommended],
@@ -19,12 +19,11 @@ export default ts.config(
     },
   },
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{jsx,tsx}'],
     extends: [
       react.configs.flat.recommended,
       react.configs.flat['jsx-runtime'],
-      reactHooks.configs['recommended-latest'],
-      reactCompiler.configs.recommended,
+      reactHooks.configs.flat.recommended,
     ],
     settings: {
       react: {
